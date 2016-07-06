66c99d2f80700ce0df7686c9e69c89ef97d77026
liuhonger <l1277314169@163.com>
2016-07-06 17:55:38 +0800

    改类

--- springMVC_Action/src/main/java/com/test.java
+++ springMVC_Action/src/main/java/com/test.java
@@ -3,7 +3,7 @@ package com;
 /**
  * Created by liuhonger on 2016/7/6.
  */
-public class test {
+public class Test {
     public static void main(String[] args) {
         System.out.println("aaa");
     }
--- springMVC_comm/src/main/java/com/student.java
+++ springMVC_comm/src/main/java/com/student.java
@@ -1,7 +1,7 @@
 /**
  * Created by liuhonger on 2016/7/6.
  */
-public class student {
+public class Student {
     public static void main(String[] args) {
         System.out.println("ggg");
     }
